83865c714656246ae9183f7e77feaf395495d796
sidbagdi <sbagdi@ucsd.edu>
2022-04-19 17:48:04 -0700

    Added test class

---
+++ MarkdownParseTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+// import MarkdownParse.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.*;
+
+public class MarkdownParseTest {
+
+    
+
+    @Test
+    public void testfile_correct() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content), List.of("https://something.com", "some-thing.html"));
+    }
+
+    @Test
+    public void testfile_infinite() throws IOException {
+        Path fileName = Path.of("test-file-infinite.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content), List.of("https://something.com", "some-thing.html"));
+    }
+
+    @Test
+    public void testfile_image() throws IOException {
+        Path fileName = Path.of("test-file-image.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content), List.of("some-thing.html"));
+    }
+
+    @Test
+    public void testfile_space() throws IOException {
+        Path fileName = Path.of("test-file-space.md");
+        String content = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(content), List.of("https://something.com"));
+    }
+}
